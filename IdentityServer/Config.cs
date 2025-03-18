fef95c104830668e54011f0cf2dc1536b1fbdf3e
İsmail Baran <ismailbaran04@gmail.com>
2025-03-18 22:22:51 +0300

    jwt oluşturmak için gerekli config ayarları eklendi

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -2,7 +2,9 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
+using Microsoft.AspNetCore.Http.Connections;
 using System.Collections.Generic;
 
 namespace IdentityServer
@@ -17,7 +19,53 @@ namespace IdentityServer
             new ApiResource("ResourceReview"){Scopes = {"ReviewFullPermission"}},
             new ApiResource("ResourcePayment"){Scopes = {"PaymentFullPermission"}},
             new ApiResource("ResourceBasket"){Scopes = {"BasketFullPermission"}},
-            new ApiResource("ResourceMessage"){Scopes = {"MessageFullPermission"}}
+            new ApiResource("ResourceMessage"){Scopes = {"MessageFullPermission"}},
+            new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
+        };
+        public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[]
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+            new IdentityResources.Email()
+        };
+        public static IEnumerable<ApiScope> ApiScopes => new ApiScope[]
+        {
+            new ApiScope("BookingFullPermission", "Full Authority For Booking Operations"),
+            new ApiScope("BookingReadPermission", "Reading Authority For Booking Operations"),
+            new ApiScope("BookingUpdatePermission", "Update Authority For Booking Operations"),
+            new ApiScope("ReviewFullPermission", "Full Authority For Review Operations"),
+            new ApiScope("PaymentFullPermission", "Full Authority For Payment Operations"),
+            new ApiScope("BasketFullPermission", "Full Authority For Basket Operations"),
+            new ApiScope("MessageFullPermission", "Full Authority For Message Operations"),
+            new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
+        };
+        public static IEnumerable<Client> Clients => new Client[]
+        {
+            new Client
+            {
+                ClientId = "BookingVisitorId",
+                ClientName = "Booking Visitor User",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = {new Secret("reservationsecret".Sha256())},
+                AccessTokenLifetime = 600,
+                AllowedScopes = { "BookingReadPermission" },
+                AllowAccessTokensViaBrowser = true
+            },
+            new Client
+            {
+                ClientId = "ReservationAdminId",
+                ClientName = "Reservation Admin User",
+                AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                ClientSecrets = {new Secret("reservationsecret".Sha256())},
+                AllowedScopes = { "BookingFullPermission", "BookingReadPermission", "BookingUpdatePermission", "ReviewFullPermission",
+                    "PaymentFullPermission", "BasketFullPermission", "MessageFullPermission",
+                IdentityServerConstants.LocalApi.ScopeName,
+                IdentityServerConstants.StandardScopes.Email,
+                IdentityServerConstants.StandardScopes.OpenId,
+                IdentityServerConstants.StandardScopes.Profile
+                },
+                AccessTokenLifetime = 600,
+            }
         };
     }
 }
