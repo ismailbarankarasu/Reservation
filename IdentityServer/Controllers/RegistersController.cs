8706a1bc3ecfa1cc04b354881ff8d10b80d7ef74
İsmail Baran <ismailbaran04@gmail.com>
2025-03-18 22:24:41 +0300

    register controller eklendi ve postman üzerinden test edildi

---
+++ IdentityServer/Controllers/RegistersController.cs
@@ -0,0 +1,41 @@
+﻿using IdentityServer.Dtos;
+using IdentityServer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RegistersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateRegister(UserRegisterDto userRegisterDto)
+        {
+            var values = new ApplicationUser()
+            {
+                Email = userRegisterDto.Email,
+                Name = userRegisterDto.Name,
+                Surname = userRegisterDto.Surname,
+                UserName = userRegisterDto.Username
+            };
+            var result = await _userManager.CreateAsync(values, userRegisterDto.Password);
+            if (result.Succeeded)
+            {
+                return Ok("Kullanıcı başarıyla oluşturuldu");
+            }
+            else
+            {
+                return BadRequest("Kullanıcı oluşturulurken hata meydana geldi !");
+            }
+        }
+    }
+}
